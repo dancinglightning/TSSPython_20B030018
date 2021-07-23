7b35befb92d500f87b1d31cc7a93cde8a6919266
Hanan Basheer <20b030018@iitb.ac.in>
2021-07-23 23:08:01 +0530

    Update flappy.py

--- Week 3/flappy.py
+++ Week 3/flappy.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 
 
 def pipe_animation():
-	global game_over, is_score_time
+	global game_over
 	for pipe in PIPES:
 		if pipe.top < 0:
 			flipped_pipe = pygame.transform.flip(pipe_image, False, True)
@@ -23,8 +23,8 @@ def pipe_animation():
 			game_over = True
 
 def draw_floor():
-	screen.blit(floor_image, (floor_x, 550))
-	screen.blit(floor_image, (floor_x+448, 550))	
+	screen.blit(floor_image, (floor_x, 508))
+	screen.blit(floor_image, (floor_x+448, 508))	
 
 def create_pipes():
 	pipe_y = random.choice(pipe_height)
@@ -32,27 +32,13 @@ def create_pipes():
 	bottom_pipe = pipe_image.get_rect(midtop=(467, pipe_y))
 	return top_pipe, bottom_pipe
 
-def score_update():
-	global score, is_score_time, high_score
-	if PIPES:
-		for pipe in PIPES:
-			if 65 < pipe.centerx < 69 and is_score_time:
-				score += 1
-				is_score_time = False
-
-			if pipe.left <= 0:
-				is_score_time = True
-
-	if score > high_score:
-		high_score = score
-
 # Basic setup
 pygame.init()
 clock = pygame.time.Clock()
 
 
 # Window
-WIDTH, HEIGHT = 350, 622
+WIDTH, HEIGHT = 288, 624
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 
@@ -74,13 +60,13 @@ bird_downflap = pygame.image.load("D:\\Codes\\Python\\PROJECTS\\TSSPython_20B030
 BIRDS = [bird_upflap, bird_midflap, bird_downflap]
 bird_index = 0
 BIRD_FLAP = pygame.USEREVENT
-pygame.time.set_timer(BIRD_FLAP, 200)
+pygame.time.set_timer(BIRD_FLAP, 100)
 
 bird_image = BIRDS[bird_index]
 bird_rect = bird_image.get_rect(center=(67, 622//2))
 
 bird_movement = 0
-gravity = 0.17
+gravity = 0.098
 
 
 # Pipes
@@ -98,11 +84,6 @@ game_over_image = pygame.image.load("D:\\Codes\\Python\\PROJECTS\\TSSPython_20B0
 game_over_rect = game_over_image.get_rect(center=(WIDTH//2,HEIGHT//2))
 
 
-# Score
-score = 0
-high_score = 0
-is_score_time = True
-
 # Game loop
 while True:
 	clock.tick(120)
@@ -114,15 +95,13 @@ while True:
 		if event.type == KEYDOWN:
 			if event.key == K_SPACE and not game_over:
 				bird_movement = 0
-				bird_movement = -7
+				bird_movement = -3
 
 			if event.key == K_SPACE and game_over:
 				game_over = False
 				PIPES = []
 				bird_movement = 0
 				bird_rect = bird_image.get_rect(center=(67, 622//2))
-				is_score_time = True
-				score = 0
 
 		if event.type == BIRD_FLAP:
 			bird_index += 1
@@ -155,7 +134,7 @@ while True:
 		
 		screen.blit(rotated_bird, bird_rect)
 		pipe_animation()
-		score_update()
+
 	elif game_over:
 		screen.blit(game_over_image, game_over_rect)
 
